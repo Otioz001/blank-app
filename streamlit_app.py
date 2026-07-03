68326f00d0bde6e6498c0558f01071bd99527949
Otioz001 <emmanuelotio@gmail.com>
2026-07-03 09:12:10 +0000

    added commisions display

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 st.set_page_config(page_title="MT5 Trading Analytics", layout="wide")
 
